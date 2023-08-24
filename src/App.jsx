776adac9e4015201946ea6b4222a34a24bb44026
Abu-Jayed <aj.webdevlopment@gmail.com>
2023-08-24 17:45:11 +0600

    bug fixes and tqsk completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
+import { Provider } from "react-redux";
 import "./App.css";
 import Header from "./components/Header/Header";
 import BookList from "./components/Section/BookList";
 import InputSection from "./components/Section/InputSection";
+import store from "./redux/store";
 
 function App() {
   return (
     <>
+      <Provider store={store}>
       <Header></Header>
       <InputSection></InputSection>
       <BookList></BookList>
+      </Provider>
     </>
   );
 }
